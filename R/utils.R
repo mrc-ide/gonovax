ca8f1b6659c80e037d0138e6e016d19fd5472126
Lilith Whittles <lilithwhittles@gmail.com>
2020-08-10 10:38:17 +0100

    add utils

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,20 @@
 `%||%` <- function(a, b) { # nolint
   if (is.null(a)) b else a
 }
+
+gonovax_file <- function(...) {
+  system.file(..., package = "gonovax", mustWork = TRUE)
+}
+
+read_csv <- function(...) {
+  utils::read.csv(..., check.names = FALSE, stringsAsFactors = FALSE)
+}
+
+write_csv <- function(...) {
+  utils::write.csv(..., row.names = FALSE)
+}
+
+data_frame <- function(...) {
+  data.frame(..., check.names = FALSE, stringsAsFactors = FALSE)
+}
+
