04eb7ee3628d2de00dc93e410016f15fa226deec
Lilith Whittles <lilithwhittles@gmail.com>
2020-08-12 08:15:31 +0100

    partway reversion to 2d but I've had another idea...

--- inst/odin/dualvax.R
+++ inst/odin/dualvax.R
@@ -13,71 +13,70 @@ n_par <- length(beta)
 n_group <- 2
 n_vax <- 1
 
-
 ## Core equations for transitions between compartments:
 
-deriv(U[, , ]) <- enr * q[j] * ve[k] - incid[i, j, k] - exr * U[i, j, k] +
-  nu[i] * A[i, j, k] + rho[i] * T[i, j, k]
-deriv(I[, , ]) <- incid[i, j, k] - (sigma[i] + exr) * I[i, j, k]
-deriv(A[, , ]) <- (1 - psi[i]) * sigma[i] * I[i, j, k] -
+deriv(U[, ]) <- enr * q[j] * (1 - ve) - n_UI[i, j] - exr * U[i, j] +
+  nu[i] * A[i, j] + (1 - vd) * rho[i] * T[i, j]
+deriv(I[, ]) <- n_UI[i, j] - (sigma[i] + exr) * I[i, j]
+deriv(A[, ]) <- (1 - psi[i]) * sigma[i] * I[i, j] -
   (eta[i] + nu[i] + exr) * A[i, j, k]
-deriv(S[, , ]) <- psi[i] * sigma[i] * I[i, j, k] - (mu[i] + exr) * S[i, j, k]
-deriv(T[, , ]) <- mu[i] * S[i, j, k] + eta[i] * A[i, j, k] -
-  (rho[i] + exr) * T[i, j, k]
+deriv(S[, ]) <- psi[i] * sigma[i] * I[i, j] - (mu[i] + exr) * S[i, j]
+deriv(T[, ]) <- mu[i] * S[i, j] + eta[i] * A[i, j] - (rho[i] + exr) * T[i, j]
 
-deriv(cum_incid[, , ]) <- incid[i, j, k]
+deriv(cum_incid[, , ]) <- n_UI[i, j]
 
 
 ## Update population size
-C[, , ] <- I[i, j, k] + A[i, j, k] + S[i, j, k]
-N[, , ] <- U[i, j, k] + C[i, j, k] + T[i, j, k]
+C[, ] <- I[i, j] + A[i, j] + S[i, j] 
+N[, ] <- U[i, j] + C[i, j] + T[i, j]
 
 # calculate mixing matrix, probability of infection and force of infection
-Np[, ] <- sum(N[i, j, ]) * p[j]
-m[, , ] <- epsilon[i] * (j == k) + (1 - epsilon[i]) * Np[i, k] / sum(Np[i, ])
-foi[, , ] <- beta[i] * p[j] * m[i, j, k] * sum(C[i, k, ]) / sum(N[i, k, ])
-incid[, , ] <- sum(foi[i, j, ]) * (1 - eff[k]) * U[i, j, k]
+Np[, ] <- sum(N[i, j]) * p[j]
+m[, , ] <- epsilon[i] * (j == k) + (1 - epsilon[i]) * Np[i] / sum(Np[i, ])
+foi[, , ] <- beta[i] * p[j] * m[i, j, k] * sum(C[i, k]) / sum(N[i, k])
+n_UI[, ] <- sum(foi[i, j, ]) * U[i, j]
+n_BI[, ] <- sum(foi[i, j, ]) * eff * BU[i, j]
+
 
 ## Set up compartments
 ## Initial states are all 0 as we will provide a state vector
-initial(U[, , ]) <- U0[i, j, k]
-initial(I[, , ]) <- I0[i, j, k]
-initial(A[, , ]) <- A0[i, j, k]
-initial(S[, , ]) <- S0[i, j, k]
-initial(T[, , ]) <- T0[i, j, k]
+initial(U[, ]) <- U0[i, j]
+initial(I[, ]) <- I0[i, j]
+initial(A[, ]) <- A0[i, j]
+initial(S[, ]) <- S0[i, j]
+initial(T[, ]) <- T0[i, j]
 
-U0[, , ] <- user()
-I0[, , ] <- user()
-A0[, , ] <- user()
-S0[, , ] <- user()
-T0[, , ] <- user()
+U0[, ] <- user()
+I0[, ] <- user()
+A0[, ] <- user()
+S0[, ] <- user()
+T0[, ] <- user()
 
-initial(cum_incid[, , ]) <- 0
+initial(cum_incid[, ]) <- 0
 
 # set up dimensions of compartments
-dim(U) <- c(n_par, n_group, n_vax)
-dim(I) <- c(n_par, n_group, n_vax)
-dim(A) <- c(n_par, n_group, n_vax)
-dim(S) <- c(n_par, n_group, n_vax)
-dim(T) <- c(n_par, n_group, n_vax)
+dim(U) <- c(n_par, n_group)
+dim(I) <- c(n_par, n_group)
+dim(A) <- c(n_par, n_group)
+dim(S) <- c(n_par, n_group)
+dim(T) <- c(n_par, n_group)
 
-dim(U0) <- c(n_par, n_group, n_vax)
-dim(I0) <- c(n_par, n_group, n_vax)
-dim(A0) <- c(n_par, n_group, n_vax)
-dim(S0) <- c(n_par, n_group, n_vax)
-dim(T0) <- c(n_par, n_group, n_vax)
+dim(U0) <- c(n_par, n_group)
+dim(I0) <- c(n_par, n_group)
+dim(A0) <- c(n_par, n_group)
+dim(S0) <- c(n_par, n_group)
+dim(T0) <- c(n_par, n_group)
 
-dim(C)  <- c(n_par, n_group, n_vax)
-dim(N)  <- c(n_par, n_group, n_vax)
+dim(C)  <- c(n_par, n_group)
+dim(N)  <- c(n_par, n_group)
 dim(Np) <- c(n_par, n_group)
 
-dim(m)     <- c(n_par, n_group, n_group)
-dim(incid) <- c(n_par, n_group, n_vax)
-dim(foi)   <- c(n_par, n_group, n_group)
+dim(m)    <- c(n_par, n_group, n_group)
+dim(foi)  <- c(n_par, n_group, n_group)
+dim(n_UI) <- c(n_par, n_group)
+dim(cum_incid) <- c(n_par, n_group)
 
-dim(cum_incid) <- c(n_par, n_group, n_vax)
-
-## Parameters
+# Parameters
 q[] <- user()
 enr <- user()
 exr <- user()
@@ -92,8 +91,11 @@ mu[] <- user()
 rho[] <- user()
 
 ## vaccination pars
-ve[] <- user()
-eff[] <- user()
+ve <- user()
+vd <- user()
+va <- user()
+eff <- user()
+w <- user()
 
 
 ## par dimensions
@@ -106,9 +108,6 @@ dim(eta) <- n_par
 dim(nu) <- n_par
 dim(mu) <- n_par
 dim(rho) <- n_par
-
-dim(ve) <- n_vax
-dim(eff) <- n_vax
 
 dim(p)   <- n_group
 
