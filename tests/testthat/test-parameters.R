a00dceb5338a5f107900827bb442b9f00a4e9f2a
Lilith Whittles <lilithwhittles@gmail.com>
2020-08-16 16:24:30 +0100

    move tests to novax

--- tests/testthat/test-parameters.R
+++ tests/testthat/test-parameters.R
@@ -12,32 +12,3 @@ test_that("can select a specific parameter sets", {
   # check that cannot extend beyond parameter set
   expect_equal(gono_params(c(1, nrow(gp) + 1)), gp[1, ])
 })
-
-test_that("novax_equilib returns equlibrium conditions", {
-
-  y <- novax_equilib(1)
-  tt <- y$t
-  params <- model_params(gono_params = gono_params(1))
-  mod <- model(user = params)
-
-  y1 <- mod$run(tt)
-  y1 <- mod$transform_variables(y1)
-
-  expect_equal(y, y1)
-# check that can load all parameters at once
-  y2 <- novax_equilib()
-  expect_equal(length(y2), 982)
-  expect_equal(y2[[1]], y1)
-
-})
-
-test_that("novax_baseline returns baseline conditions", {
-
-  y <- novax_baseline(1:3, 1:10)
-  expect_equal(names(y), c("incid", "cum_incid"))
-  expect_equal(dim(y$incid), c(10, 3))
-  expect_equal(dim(y$cum_incid), c(10, 3))
-  expect_equal(colSums(y$incid), y$cum_incid[10, ])
-
-  expect_error(novax_baseline(1, 11), "t must be an integer between 1 and 10")
-})
