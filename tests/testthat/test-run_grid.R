b54026b82f719fef06156f7f363054f81a5c19e6
Lilith Whittles <lilithwhittles@gmail.com>
2020-08-25 11:39:22 +0100

    move grid tests

---
+++ tests/testthat/test-run_grid.R
@@ -0,0 +1,52 @@
+
+test_that("run_grid_onevax works as expected", {
+  y <- run_grid_onevax(n = 2, t = 2, eff = c(0, 1), dur = c(1, 2), ve = 0.5,
+                       full_output = TRUE)
+  expect_equal(y$red_incid[, 1], c(0, 0), tol = 0.1)
+  expect_equal(y$red_incid[, 1], y$red_incid[, 3], tol = 0.1)
+  expect_true(all(y$red_incid > -0.1))
+  expect_equal(y$cum_red_incid[, 1], c(0, 0), tol = 0.1)
+  expect_equal(y$red_incid[, 1], y$red_incid[, 3], tol = 0.1)
+  expect_true(all(abs(y$cum_vaccinated - 1200 * 10 * 0.5 * 2) < 1e-5))
+  expect_equal(length(y$results), nrow(y$inputs$grid))
+  plot(y)
+  
+  y2 <- run_grid_onevax(n = 2, t = 2, eff = c(0, 1), dur = c(1, 2), vd = 1)
+  expect_equal(y2$red_incid[, 1], c(0, 0), tol = 0.1)
+  expect_equal(y2$red_incid[, 1], y2$red_incid[, 3], tol = 0.1)
+  expect_true(all(y2$red_incid > -0.1))
+  expect_equal(y2$cum_red_incid[, 1], c(0, 0), tol = 0.1)
+  expect_equal(y2$red_incid[, 1], y2$red_incid[, 3], tol = 0.1)
+  plot(y2)
+  ### this looks fine
+  y3 <- run_grid_onevax(n = 2, t = 2, eff = c(0, 1), dur = c(1, 2), vs = 1)
+  expect_equal(y3$red_incid[, 1], c(0, 0), tol = 0.1)
+  expect_equal(y3$red_incid[, 1], y3$red_incid[, 3], tol = 0.1)
+  expect_equal(y3$cum_red_incid[, 1], c(0, 0), tol = 0.1)
+  expect_equal(y3$cum_red_incid[, 1], y3$cum_red_incid[, 3], tol = 0.1)
+  expect_true(all(y3$red_incid > -0.1))
+  plot(y3)
+  
+  y0 <- run_grid_onevax(n = 2, t = 2, eff = c(0, 1), dur = c(1, 2))
+  # runs ok with user baseline
+  y4 <- run_grid_onevax(n = 2, t = 2, eff = c(0, 1), dur = c(1, 2), ve = 0.5,
+                        baseline = y0)
+  expect_equal(y4$red_incid, y$red_incid)
+  expect_equal(y4$cum_red_incid, y$cum_red_incid)
+  
+  # check error cases
+  expect_error(run_grid_onevax(n = 3, t = 2, eff = c(0, 1), dur = c(1, 2),
+                               ve = 0.5, baseline = y0),
+               "model parameters do not match baseline")
+  expect_error(run_grid_onevax(n = 2, t = 2, eff = c(0, 1), dur = c(1, 3),
+                               ve = 0.5, baseline = y0),
+               "dur / eff parameters do not match baseline")
+  expect_error(run_grid_onevax(n = 2, t = 3, eff = c(0, 1), dur = c(1, 2),
+                               ve = 0.5, baseline = y0),
+               "t does not match baseline")
+  class(y0) <- NULL
+  expect_error(run_grid_onevax(n = 2, t = 2, eff = c(0, 1), dur = c(1, 2),
+                               ve = 0.5, baseline = y0),
+               "baseline must be a gonovax_grid object")
+  
+})
