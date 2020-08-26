e1bff7e19a5015275684f368e5026f2e29c7f85b
Lilith Whittles <lilithwhittles@gmail.com>
2020-08-26 10:06:31 +0100

    add tests

--- tests/testthat/test-parameters.R
+++ tests/testthat/test-parameters.R
@@ -12,3 +12,41 @@ test_that("can select a specific parameter sets", {
   # check that cannot extend beyond parameter set
   expect_equal(gono_params(c(1, nrow(gp) + 1)), gp[1, ])
 })
+
+test_that("vax_map works correctly", {
+  # check error when input more than n_group vaccine uptake
+  expect_error(create_vax_map(2, rep(0, 3)))
+  # check error when uptake not in 0-1
+  expect_error(create_vax_map(2, c(0, 2)))
+  expect_error(create_vax_map(2, c(0, -1)))
+
+  # indices of y [group, out, in]
+  # test onevax map
+  y <- create_vax_map(2, c(0.1, 0.2))
+  expect_equal(y[1, 1, 1], 0.1)
+  expect_equal(y[2, 1, 1], 0.2)
+  expect_equal(sum(y[, , -1]), 0)
+  expect_true(all(apply(y, c(1, 3), sum) == 0))
+
+  # test onevax waning map
+  y <- create_vax_map(5, c(0.1, 0.2))
+  expect_equal(y[1, 1, 1], 0.1)
+  expect_equal(y[2, 1, 1], 0.2)
+  expect_equal(sum(y[, , -1]), 0)
+  expect_true(all(apply(y, c(1, 3), sum) == 0))
+})
+
+test_that("waning map works correctly", {
+
+  expect_error(create_waning_map(2, 1, -1))
+
+  # test onevax map
+  y <- create_waning_map(2, 1, 1)
+  expect_equal(rowSums(y), c(1, -1))
+  expect_equal(colSums(y), c(0, 0))
+
+  # test onevax waning map
+  y <- create_waning_map(5, 5, 1)
+  expect_equal(rowSums(y), c(0, -1, 0, 0, 1))
+  expect_equal(colSums(y), c(0, 0, 0, 0, 0))
+})
