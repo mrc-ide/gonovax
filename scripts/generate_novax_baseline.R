5dbd587d7c2484f420d1f8a495fa3325dff3ce63
Lilith Whittles <lilithwhittles@gmail.com>
2020-08-28 09:17:12 +0100

    change comment to satisfy codefactor

--- scripts/generate_novax_baseline.R
+++ scripts/generate_novax_baseline.R
@@ -6,7 +6,7 @@ res <- gonovax::run_novax(tt = seq(0, 20), equilib = TRUE)
 cum_incid <- sapply(res, function(x) apply(x[["cum_incid"]], 1, sum))
 
 # extract incidence from cumulative incidence and remove cumulative incidence at
-# t = 0
+# time zero
 novax_baseline <- list(incid = apply(cum_incid, 2, diff),
                        cum_incid = cum_incid[-1, ])
 
