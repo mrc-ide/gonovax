edb97ab7651a23c9416ebbc52c831fbcdb0464e8
Lilith Whittles <lilithwhittles@gmail.com>
2020-09-11 10:28:35 +0100

    extract vaccination and revaccination flows separately this requires allowing aggregation by strata

--- R/support.R
+++ R/support.R
@@ -3,12 +3,16 @@
 ##' @param x a transformed model run output
 ##' @param what a character string of the cumulative trajectory to transform
 ##' @param as_incid logical specifying whether to convert to incidence
+##' @param stratum an integer denoting the stratum to aggregate over, if NULL
+##' aggregation is over all strata
 ##' @param f function to apply across parameter sets (eg. mean / median)
 ##' @param ... named arguments to pass to f
 ##' @return a transformed time series / array
 ##' @export
-aggregate <- function(x, what, as_incid = FALSE, f = identity, ...) {
-  y <- sapply(x, function(x) apply(x[[what]], 1, sum))
+aggregate <- function(x, what, as_incid = FALSE, stratum = NULL,
+                      f = identity, ...) {
+  strata <- stratum %||% seq_len(dim(x[[1]][[what]])[3])
+  y <- sapply(x, function(x) apply(x[[what]][, , strata], 1, sum))
   if (as_incid) {
     dt <- diff(x[[1]]$t)
     y <- apply(y, 2, diff) / dt
@@ -17,13 +21,24 @@ aggregate <- function(x, what, as_incid = FALSE, f = identity, ...) {
 }
 
 extract_flows <- function(y) {
+
   # extract cumulative flows
-  flow_names <- grep(pattern = "^cum", names(y[[1]]), value = TRUE)
-  cumulative_flows <- lapply(flow_names, function(x) t(aggregate(y, x))[-1, ])
+  flow_names <- c("cum_incid", "cum_diag_a", "cum_diag_s",
+                  "cum_treated", "cum_screened")
+  cumulative_flows <- lapply(flow_names, function(x) t(aggregate(y, x)))
   names(cumulative_flows) <- flow_names
 
-  # extract flows
-  flows <- lapply(flow_names, function(x) t(aggregate(y, x, TRUE)))
-  names(flows) <- gsub("^cum_", "", flow_names)
+  # extract vaccinations and revaccinations separately
+  what <- "cum_vaccinated"
+  cumulative_flows$cum_vaccinated <- t(aggregate(y, what, stratum = 1))
+  cumulative_flows$cum_revaccinated <- t(aggregate(y, what, stratum = 3))
+
+  # extract annual flows
+  flows <- lapply(cumulative_flows, function(x) apply(x, 2, diff))
+  names(flows) <- gsub("^cum_", "", names(cumulative_flows))
+
+  # remove time 0 from cumulative flows
+  cumulative_flows <- lapply(cumulative_flows, "[", -1, )
+
   c(cumulative_flows, flows)
 }
--- tests/testthat/test-support.R
+++ tests/testthat/test-support.R
@@ -1,7 +1,7 @@
 test_that("aggregate works", {
   dt <- 1 / 12
   tt <- seq(0, 1, dt)
-  y <- run_onevax(1:2, tt, eff = 1, dur = 4)
+  y <- run_onevax(1:2, tt, eff = 1, dur = 4, vd = 1)
   z <- aggregate(y, "cum_incid", FALSE)
   expect_equal(dim(z), c(2, length(tt)))
   expect_equal(z[1, ], apply(y[[1]]$cum_incid, 1, sum))
@@ -10,10 +10,26 @@ test_that("aggregate works", {
   z1 <- aggregate(y, "cum_incid", TRUE)
   expect_equal(z1, t(apply(z, 1, diff)) / dt)
 
-  z2 <- aggregate(y, "cum_incid", FALSE, mean)
+  z2 <- aggregate(y, "cum_incid", FALSE, f = mean)
   expect_equal(z2, colMeans(z))
 
-  z3 <- aggregate(y, "cum_incid", TRUE, mean)
+  z3 <- aggregate(y, "cum_incid", TRUE, f = mean)
   expect_equal(z3, colMeans(z1))
+
+  z4 <- aggregate(y, "cum_incid", TRUE, stratum = 1, f = mean)
+  z5 <- aggregate(y, "cum_incid", TRUE, stratum = 3, f = mean)
+  expect_equal(z4 + z5, z3)
+
+})
+
+test_that("extract_flows works", {
+  tt <- seq(0, 2)
+  y <- run_onevax(1:2, tt, eff = 1, dur = 4, vd = 1)
+  z <- extract_flows(y)
+
+  expect_equal(z$cum_incid[1, ], z$incid[1, ])
+  expect_equal(z$cum_incid[2, ] - z$cum_incid[1, ], z$incid[2, ])
+  expect_equal(z$vaccinated + z$revaccinated,
+               t(aggregate(y, "cum_vaccinated", as_incid = TRUE)))
 
 })
