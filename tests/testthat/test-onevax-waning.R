5578cf9fc3bc83bb0b44acf6c10c62316ddd8c90
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2020-11-24 11:26:24 +0000

    Eliminate warnings

--- tests/testthat/test-onevax-waning.R
+++ tests/testthat/test-onevax-waning.R
@@ -17,20 +17,20 @@ test_that("run_onevax_waning works correctly", {
                            equilib = TRUE)[[1]]
 
   expect_equal(y2$cum_vaccinated, y2e$cum_vaccinated)
-  expect_equal(apply(y2e$N, 1, sum), rep(6e5, 4), tol = 1e-5)
+  expect_equal(apply(y2e$N, 1, sum), rep(6e5, 4), tolerance = 1e-5)
 
   # check vaccination on treatment is working correctly
   y3e <- run_onevax_waning(1, tt, eff = 1, dur = 1, vd = 1,
                            equilib = TRUE)[[1]]
   expect_equal(y3e$cum_vaccinated[, , 1], y3e$cum_treated[, , 1])
   expect_equal(sum(y3e$cum_treated[, , 2]), 0)
-  expect_equal(apply(y3e$N, 1, sum), rep(6e5, 4), tol = 1e-5)
+  expect_equal(apply(y3e$N, 1, sum), rep(6e5, 4), tolerance = 1e-5)
 
   # check vaccination on screening is working correctly
   y4e <- run_onevax_waning(1, tt, eff = 1, dur = 1, vs = 1,
                            equilib = TRUE)[[1]]
   expect_equal(y4e$cum_vaccinated[, , 1], y4e$cum_screened[, , 1])
-  expect_equal(apply(y4e$N, 1, sum), rep(6e5, 4), tol = 1e-5)
+  expect_equal(apply(y4e$N, 1, sum), rep(6e5, 4), tolerance = 1e-5)
   # check that number of vaccinations is lower with no revaccination
   # this is fine for vs but, more complex for vd as incidence affects vax cov
   z <- run_onevax(1, tt, eff = 1, dur = 1, vs = 1,
@@ -40,14 +40,14 @@ test_that("run_onevax_waning works correctly", {
   # check waned vaccines move into correct compartment
   y5e <- run_onevax_waning(1, tt, eff = 1, dur = 1, ve = 1,
                            equilib = TRUE)[[1]]
-  expect_equal(apply(y5e$N, 1, sum), rep(6e5, 4), tol = 1e-5)
+  expect_equal(apply(y5e$N, 1, sum), rep(6e5, 4), tolerance = 1e-5)
   expect_true(all(apply(y5e$N[-1, , 3], 1, sum) > 0))
   # check that equivalent to waning into U
   z <- run_onevax(1, tt, eff = 1, dur = 1, ve = 1,
                          equilib = TRUE)[[1]]
-  expect_equal(z$N[, , 1], y5e$N[, , 1] + y5e$N[, , 3], tol = 0.1)
+  expect_equal(z$N[, , 1], y5e$N[, , 1] + y5e$N[, , 3], tolerance = 0.1)
   expect_equal(z$cum_incid[, , 1], y5e$cum_incid[, , 1] + y5e$cum_incid[, , 3],
-               tol = 0.1)
+               tolerance = 0.1)
 
   # check vaccination targeting
   y6e <- run_onevax_waning(1, tt, eff = 1, dur = 1,
