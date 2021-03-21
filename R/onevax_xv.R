09be8b10b1af1698b2eb5dbed8297054e0be8130
Lilith Whittles <lilithwhittles@gmail.com>
2021-03-21 11:45:42 +0000

    move run_onevax_xv into package

--- R/onevax_xv.R
+++ R/onevax_xv.R
@@ -1,5 +1,5 @@
-##' @name vax_params1
-##' @title create vaccination parameters for use in onevax model
+##' @name vax_params_xv
+##' @title create vaccination parameters for use in onevax_xv model
 ##' @param eff single numeric indicating efficacy of the vaccine (between 0-1)
 ##' @param dur single numeric indicating duration of the vaccine (in years)
 ##' @param ve single numeric indicating % of population vaccinated before entry
@@ -12,7 +12,7 @@
 ##' length 2 will apply to each activity group separately
 ##' @param t_stop time at which vaccination should stop (years)
 ##' @return A list parameters in the model input format
-vax_params1 <- function(eff = 0, dur = 1e3,
+vax_params_xv <- function(eff = 0, dur = 1e3,
                         ve = 0, vs = 0, vd = 0, t_stop = 99) {
   # waned vaccinees move to own compartment, but are eligible for re-vaccination
   i_u <- c(1, 3)
@@ -29,35 +29,10 @@ vax_params1 <- function(eff = 0, dur = 1e3,
   )
 }
 
-run_onevax_int <- function(n, tt, eff, dur, ve, vd, vs, t_stop, equilib) {
-
-  if (length(n) != 1) stop("length(n) must equal 1")
-
-  vax_params <- vax_params1(eff = eff, dur = dur,
-                            ve = ve, vs = vs, vd = vd, t_stop = t_stop)
-
-  if (equilib) {
-    init_params <- restart_params(novax_equilib(n), n_vax = vax_params$n_vax)
-  } else {
-    # set inital params based on gono_params() input
-    init_params <- NULL
-  }
-
-  pars <- model_params(gono_params = gono_params(n),
-                       init_params = init_params,
-                       vax_params = vax_params)
-  mod <- model(user = pars, unused_user_action = FALSE)
-  y <- mod$run(tt)
-  mod$transform_variables(y)
-}
-
-##' @name run_onevax
+##' @name run_onevax_xv
 ##' @title run model with single vaccine for input parameter sets, either from
 ##' initialisation or from equilibrium
-##' @param n an integer vector (or value) containing the indices of
-##' corresponding parameter set (1:982). If `n = NULL` the equilibrium positions
-##' for the full parameter set are returned
-##' @param tt a numeric vector of times at which the model state is output
+##' @inheritParams run
 ##' @param eff single numeric indicating efficacy of the vaccine (between 0-1)
 ##' @param dur single numeric indicating duration of the vaccine (in years)
 ##' @param ve single numeric indicating % of population vaccinated before entry
@@ -69,16 +44,14 @@ run_onevax_int <- function(n, tt, eff, dur, ve, vd, vs, t_stop, equilib) {
 ##' (between 0-1) a scalar will apply to both activity groups, a vector of
 ##' length 2 will apply to each activity group separately
 ##' @param t_stop time at which vaccination should stop (years)
-##' @param equilib a logical indicating whether to run from equilibrium, default
-##' is `FALSE`, i.e. run from initial conditions
 ##' @return A list of transformed model outputs
 ##' @export
-run_onevax <- function(n = NULL, tt, eff, dur,
-                       ve = 0, vd = 0, vs = 0, t_stop = 99,
-                       equilib = FALSE) {
-  n <- n %||% seq_len(nrow(gono_params()))
-  lapply(n, run_onevax_int,
-         tt = tt, eff = eff, dur = dur,
-         ve = ve, vd = vd, vs = vs, t_stop = t_stop,
-         equilib = equilib)
+run_onevax_xv <- function(tt, gono_params, init_params,
+                          eff, dur, ve = 0, vd = 0, vs = 0,
+                          t_stop = 99) {
+  
+  vax_params <- Map(vax_params_xv, eff = eff, dur = dur,
+                    ve = ve, vs = vs, vd = vd, t_stop = t_stop)
+  Map(run, gono_params = gono_params, init_params = init_params,
+      vax_params = vax_params, MoreArgs = list(tt = tt))
 }
