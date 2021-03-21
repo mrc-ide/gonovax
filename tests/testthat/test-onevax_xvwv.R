5292cf041a015c82639f66cc9edb390cb4a71624
Lilith Whittles <lilithwhittles@gmail.com>
2021-03-21 12:12:46 +0000

    update docs

--- tests/testthat/test-onevax_xvwv.R
+++ tests/testthat/test-onevax_xvwv.R
@@ -2,39 +2,38 @@ context("onevax")
 
 test_that("run_onevax works correctly", {
   tt <- seq(0, 5)
-  y1 <- run_onevax(1, tt, eff = 0, dur = 1e3)[[1]]
-  expect_error(run_onevax_int(1:2, tt),
-               label = "length(n) must equal 1")
+  y1 <- run_onevax_xvwv(1, tt, eff = 0, dur = 1e3)[[1]]
+
   # check no-one is vaccinated with v switched off
   expect_true(all(y1$cum_vaccinated == 0))
-  y2 <- run_onevax(1, tt, eff = 0, dur = 1e3, ve = 1)[[1]]
+  y2 <- run_onevax_xvwv(1, tt, eff = 0, dur = 1e3, ve = 1)[[1]]
   # check 100% vbe vaccinates all new entrants
   expect_equal(diff(rowSums(y2$cum_vaccinated[, , 1])), rep(12e3, max(tt)))
   # and no-one else
   expect_equal(sum(y2$cum_vaccinated[, , 2:3]), 0)
 
   # check can run from equilib
-  y2e <- run_onevax(1, tt, eff = 0, dur = 1e3, ve = 1, equilib = TRUE)[[1]]
+  y2e <- run_onevax_xvwv(1, tt, eff = 0, dur = 1e3, ve = 1, equilib = TRUE)[[1]]
 
   expect_equal(y2$cum_vaccinated, y2e$cum_vaccinated)
   expect_equal(apply(y2e$N, 1, sum), rep(6e5, 6), tolerance = 1e-5)
 
   # check vaccination on treatment is working correctly
-  y3e <- run_onevax(1, tt, eff = 1, dur = 1e3, vd = 1, equilib = TRUE)[[1]]
+  y3e <- run_onevax_xvwv(1, tt, eff = 1, dur = 1e3, vd = 1, equilib = TRUE)[[1]]
   expect_equal(y3e$cum_vaccinated[, , 1], y3e$cum_treated[, , 1])
   expect_equal(y3e$cum_vaccinated[, , 3], y3e$cum_treated[, , 3])
   expect_equal(sum(y3e$cum_treated[, , 2]), 0)
   expect_equal(apply(y3e$N, 1, sum), rep(6e5, 6), tolerance = 1e-5)
 
   # check vaccination on screening is working correctly
-  y4e <- run_onevax(1, tt, eff = 1, dur = 1e3, vs = 1, equilib = TRUE)[[1]]
+  y4e <- run_onevax_xvwv(1, tt, eff = 1, dur = 1e3, vs = 1, equilib = TRUE)[[1]]
   expect_equal(y4e$cum_vaccinated[, , 1], y4e$cum_screened[, , 1])
   expect_equal(sum(y4e$cum_treated[, , 2]), 0)
   expect_equal(y4e$cum_vaccinated[, , 3], y4e$cum_screened[, , 3])
   expect_equal(apply(y4e$N, 1, sum), rep(6e5, 6), tolerance = 1e-5)
 
   # check vaccination targeting
-  y5e <- run_onevax(1, tt, eff = 1, dur = 1e3,
+  y5e <- run_onevax_xvwv(1, tt, eff = 1, dur = 1e3,
                     vs = c(0, 1), vd = c(0, 1), equilib = TRUE)[[1]]
   expect_equal(y5e$N[, 1, 2], rep(0, 6))
   expect_equal(sum(y5e$cum_vaccinated[, 1, ]), 0)
