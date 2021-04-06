c95c193ea078ca9d0713d942fdb1119944d4cf52
Lilith Whittles <lilithwhittles@gmail.com>
2021-04-06 10:47:31 +0100

    fix gono_params tests

--- tests/testthat/test-parameters.R
+++ tests/testthat/test-parameters.R
@@ -1,17 +1,18 @@
 context("parameters")
 
-test_that("can select a specific parameter sets", {
-  gp <- read_csv(gonovax_file("extdata/gono_params.csv"))
+test_that("can select specific parameter sets", {
+  p <- read_csv(gonovax_file("extdata/gono_params.csv"))
+  gp <- lapply(seq_len(nrow(p)), function(i) transform0(p[i, ]))
   # check that null argument returns all params
   expect_equal(gono_params(), gp)
   # check that can extract a single parameter
-  expect_equal(gono_params(1), gp[1, ])
+  expect_equal(gono_params(1)[[1]], transform0(p[1, ]))
   # check that can extract multiple parameters
-  expect_equal(gono_params(2:3), gp[2:3, ])
+  expect_equal(gono_params(2:3), gp[2:3])
   # check that negative paramters will not be returned
-  expect_equal(gono_params(c(-1, 1)), gp[1, ])
+  expect_equal(gono_params(c(-1, 1)), p[1, ])
   # check that cannot extend beyond parameter set
-  expect_equal(gono_params(c(1, nrow(gp) + 1)), gp[1, ])
+  expect_equal(gono_params(c(1, nrow(p) + 1)), gp[1])
 })
 
 test_that("vax_map works correctly", {
