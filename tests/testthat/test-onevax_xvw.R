dea71c20ccc802f651713ddfa18607c2e32a7056
Lilith Whittles <lilithwhittles@gmail.com>
2021-07-16 16:27:03 +0100

    de-lint

--- tests/testthat/test-onevax_xvw.R
+++ tests/testthat/test-onevax_xvw.R
@@ -113,7 +113,7 @@ test_that("can set initial coverage", {
   y <- run_onevax_xvw(tt, gp, ves = 1, dur = 1e3, coverage = 0.33)[[1]]
   expect_equivalent(apply(y$N[, , -1], c(1, 2), sum) / apply(y$N, c(1, 2), sum),
                     rep(cov, length(tt) * 2))
-  
+
   expect_error(run_onevax_xvw(tt, gp, ves = 1, dur = 1e3, coverage = c(0.1, 0)),
                "'coverage' must be a scalar")
 })
