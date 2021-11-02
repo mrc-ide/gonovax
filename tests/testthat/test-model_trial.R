cc8cba82fe9749839be2e3654ec51bc587e34c96
Dariyanikitin <dn620@ic.ac.uk>
2021-11-02 13:03:47 +0000

    scans the low activity group and makes sure its empty and that i've coded the shift to high activity correctly!

--- tests/testthat/test-model_trial.R
+++ tests/testthat/test-model_trial.R
@@ -44,9 +44,23 @@ test_that("number of individuals always >= 0", {
   
 })
 
+test_that("all individuals are in the high activity group", {
 
-
-
+params <- gonovax::model_params_trial(gono_params_trial = gonovax::gono_params_trial(1)[[1]])
+params$lambda <- 1.5                                                           
+mod <- model_trial$new(user = params, unused_user_action = "ignore")     
+tt <- seq.int(0, 5) / 365                                             
+y <- mod$run(t = tt)                                                 
+y <- mod$transform_variables(y)                                                
+  
+#low activity group empty
+expect_true(all(y$U[ , 1, 1] ==0))
+expect_true(all(y$I[ , 1, 1] ==0))
+expect_true(all(y$A[ , 1, 1] ==0))
+expect_true(all(y$S[ , 1, 1] ==0))
+expect_true(all(y$T[ , 1, 1] ==0))
+  
+})
 
 
 
