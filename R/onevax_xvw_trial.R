f1b310dc3a86e2c6b1343e7c6b4b12b05b7c07e5
Dariya Nikitin <dn620@ic.ac.uk>
2021-11-10 17:37:36 +0000

    cleaned up code grammar with lintr

--- R/onevax_xvw_trial.R
+++ R/onevax_xvw_trial.R
@@ -1,4 +1,5 @@
-#initial_params_xvw
+## sets up the starting conditions for the state variables in the presence of
+## vaccination
 
 initial_params_xvw_trial <- function(pars, coverage = 0) {
   assert_scalar_unit_interval(coverage)
@@ -8,42 +9,30 @@ initial_params_xvw_trial <- function(pars, coverage = 0) {
  initial_params_trial(pars, n_vax, cov)
 }
 
-#test
-#initial_params_xvw_trialset <- initial_params_xvw_trial(params, coverage = 0.5)             #works! 
 
-### initial conditions = 3 stratum, with 50% in non vaccinated, 50% in vaccinated 
-
+## sets up vaccination efficacies, who experiences the effects of vaccination,
+## how waning occurs
 
 vax_params_xvw_trial <- function(vea = 0, vei = 0, ved = 0, ves = 0,
                            dur = 1e3,
                            t_stop = 99) {
-  
- # assert_character(strategy)
+
   assert_scalar_unit_interval(vea)
   assert_scalar_unit_interval(vei)
   assert_scalar_unit_interval(ved)
   assert_scalar_unit_interval(ves)
   assert_scalar_positive(dur)
- # assert_scalar_unit_interval(uptake)
- # assert_scalar_unit_interval(vbe)
   assert_scalar_positive(t_stop)
-  
+
   # waned vaccinees move to own stratum, and are not eligible for re-vaccination
-  # 1:x -> 2:v <-> 3:w
-  # i_eligible <- 1
    i_v <- 2
    i_w <- n_vax <- 3
-   
+
   # compartments to which vaccine efficacy applies
   ve <- c(0, 1, 0)
   ved <- min(ved, 1 - 1e-10) # ensure duration is not divided by 0
-  
-#  p <- set_strategy(strategy, uptake)
-  
+
   list(n_vax = n_vax,
-    #   vbe   = create_vax_map(n_vax, vbe, i_eligible, i_v),
-      # vod   = create_vax_map(n_vax, p$vod, i_eligible, i_v),
-       # vos   = create_vax_map(n_vax, p$vos, i_eligible, i_v),
        vea   = vea * ve,
        vei   = vei * ve,
        ved   = ved * ve,
@@ -55,58 +44,32 @@ vax_params_xvw_trial <- function(vea = 0, vei = 0, ved = 0, ves = 0,
 }
 
 
-##' @name run_onevax_xvw
-##' @title Run model with single vaccine for input parameter sets, either from
-##' initialisation or from equilibrium, those with waned vaccines are not
-##' eligible for revaccination.
-##' @param gono_params list of gono params
-##' @param vea scalar or numeric vector with same length as `gono_params` giving
-##'  efficacy of the vaccine against acquisition (between 0-1)
-##' @param vei scalar or numeric vector with same length as `gono_params` giving
-##'  efficacy of the vaccine against infectiousness (between 0-1)
-##' @param ved scalar or numeric vector with same length as `gono_params` giving
-##'  efficacy of the vaccine against duration (between 0-1)
-##' @param ves scalar or numeric vector with same length as `gono_params` giving
-##'  efficacy of the vaccine against symptoms (between 0-1)
-##' @param vbe scalar giving uptake of vaccination before
-##' entry into population (i.e. adolescent vaccination) defaults to same as
-##' `coverage`
-##' @param dur  scalar or numeric vector with same length as `gono_params`
-##'  giving duration of the vaccine (in years)
-##' @param uptake  scalar or numeric vector with same length as `gono_params`
-##'  giving pc of population vaccinated as part of strategy
-##' @param coverage scalar giving initial coverage of vaccination, default 0.
-##' @inheritParams run
-##' @inheritParams vax_params_xvw
-##' @export
+## runs the trial model when supplied NHPs
 
-run_onevax_xvw_trial <- function(tt, gono_params, initial_params_trial = NULL, dur = 1e3,    #duration here is where we can establish waning (1/dur??)
-                           vea = 0, vei = 0, ved = 0, ves = 0, 
-                           coverage = 0, 
+run_onevax_xvw_trial <- function(tt, gono_params, initial_params_trial = NULL,
+                           dur = 1e3,
+                           vea = 0, vei = 0, ved = 0, ves = 0,
+                           coverage = 0,
                            t_stop = 99) {
- 
+
   stopifnot(all(lengths(list(vea, vei, ved, ves, dur)) %in%
                   c(1, length(gono_params))))
   assert_scalar_unit_interval(coverage)
-  
+
   vax_params <- Map(vax_params_xvw_trial, dur = dur,
                     vea = vea, vei = vei, ved = ved, ves = ves,
                     MoreArgs = list(t_stop = t_stop))
 
-  if (is.null(initial_params_trial)) {                         #if initial_params not provided it will generate them   
+  if (is.null(initial_params_trial)) {
     pars <- lapply(gono_params, model_params_trial)
-    init_params_trial <- Map(initial_params_xvw_trial, pars = pars, coverage = coverage)
+    ipt <- Map(initial_params_xvw_trial, pars = pars, coverage = coverage)
+    init_params_trial <- ipt
   }
- 
-  ret <- Map(run_trial, gono_params = gono_params, init_params = init_params_trial,    # why is demographic params not included in this ?
-             vax_params = vax_params,                                                     #assumed to form within model_params inside the if statement? 
-             MoreArgs = list(tt = tt))                                                          #this is the bit that runs it! wait isn't this what we did before?
- 
 
+  ret <- Map(run_trial, gono_params = gono_params,
+             init_params = init_params_trial,
+             vax_params = vax_params,
+             MoreArgs = list(tt = tt))
 
-  
   ret
-} 
-#it just makes it all part of one function than us writing out multiple lines maybe??? ask lilith, but theni think vaccination did do something? 
-#note we held vaccination at 0 for all vex so that's probably why?? 
-
+}
--- R/parameters_trial.R
+++ R/parameters_trial.R
@@ -3,42 +3,40 @@
 gono_params_trial <- function(n = NULL) {
   if (is.null(cache$gono_params_trial)) {
     gp <- read_csv(gonovax_file("extdata/gono_params_updated.csv"))
-    gp$lambda <- 1.5   #come back to this later!! Your lambda will be different 
+    gp$lambda <- 1.5   #come back to this later!! Your lambda will be different
     cache$gono_params_trial <-
-      lapply(seq_len(nrow(gp)), function(i) transform0_trial(gp[i, ]))   
+      lapply(seq_len(nrow(gp)), function(i) transform0_trial(gp[i, ]))
   }
-  
+
   pars <- cache$gono_params_trial
   n_pars <- length(pars)
   # if n not supplied, return all parameters
   i  <- n %||% seq_len(n_pars)
   # limit to parameter sets available
   i <- i[(i > 0) & (i <= n_pars)]
-  
+
   pars[i]
-  
+
   }
 
 
 
-## transforms the imported parameters into workable format 
+## transforms the imported parameters into workable format
 
-transform0_trial <- function(pars) {         
-
-  pars <- as.list(pars)              #converts each row of csv into list 
-  
-  check_gono_params(pars)          
+transform0_trial <- function(pars) {
+  pars <- as.list(pars)              #converts each row of csv into list
+  check_gono_params(pars)
   with(pars, {
     assert_scalar_positive(beta)
     assert_scalar_positive(eta_h)
     assert_scalar_positive(eta_l)
   })
-  
-  
-  pars$tt <- c(0, 1e3)        
+
+
+  pars$tt <- c(0, 1e3)
 
   pars
-  
+
 }
 
 
@@ -55,18 +53,18 @@ demographic_params_trial <- function() {
 ## sets up the starting conditions for state variables of each activity group
 
 initial_params_trial <- function(pars, n_vax = 1, coverage = 1) {
-  
+
   stopifnot(length(coverage) == n_vax)
   stopifnot(sum(coverage) == 1)
-  
+
   U0 <- I0 <- A0 <- S0 <- T0 <- array(0, c(2, n_vax))
-  
+
   # separate into 1:low and 2:high activity groups and by coverage
   N0 <- pars$N0 * outer(pars$move, coverage)
-  
-  # put the vaccinated and placebo individuals all to uninfected 
+
+  # put the vaccinated and placebo individuals all to uninfected
   U0 <- round(N0)
-  
+
   list(U0 = U0, I0 = I0, A0 = A0, S0 = S0, T0 = T0)
 }
 
@@ -79,17 +77,19 @@ model_params_trial <- function(gono_params_trial = NULL,
                         initial_params_trial = NULL,
                         vax_params = NULL, coverage = 0) {
   gono_params_trial <- gono_params_trial %||% gono_params_trial(1)[[1]]
-  demographic_params_trial <- demographic_params_trial  %||% demographic_params_trial()
+  dpt <- demographic_params_trial  %||% demographic_params_trial()
+  demographic_params_trial <- dpt
   ret <- c(demographic_params_trial, gono_params_trial)
-  vax_params <- vax_params %||% vax_params0()                                          
-  
-  if (coverage == 0){
-    cov <- c(1, rep(0, vax_params$n_vax - 1))        
-    initial_params <- initial_params_trial%||% initial_params_trial(ret, vax_params$n_vax, cov)
+  vax_params <- vax_params %||% vax_params0()
+
+  if (coverage == 0) {
+    cov <- c(1, rep(0, vax_params$n_vax - 1))
+a <- initial_params_trial %||% initial_params_trial(ret, vax_params$n_vax, cov)
+    initial_params_trial <- a
 
   } else {
     initial_params <- initial_params_xvw_trial(pars = ret, coverage = coverage)
   }
-  
+
   c(ret, initial_params, vax_params)
 }
