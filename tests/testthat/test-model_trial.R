569674b2aa5871418a1adda9da4c86ebdd5abbf8
Dariya Nikitin <61972511+Dariyanikitin@users.noreply.github.com>
2021-11-17 11:43:18 +0000

    removed lambda input as part of function now change in csv
    Co-authored-by: Lilith Whittles <lwhittles@users.noreply.github.com>

--- tests/testthat/test-model_trial.R
+++ tests/testthat/test-model_trial.R
@@ -133,7 +133,6 @@ test_that("all individuals are uninfected at t = 0", {
 
 test_that("no-one is treated when mu and eta = 0", {
   params <- model_params_trial(gono_params_trial = gono_params_trial(1)[[1]])
-  params$lambda <- 1.5
   params$mu <- params$eta_h[] <- params$eta_l[] <-  0
   mod <- model_trial$new(user = params, unused_user_action = "ignore")
 
