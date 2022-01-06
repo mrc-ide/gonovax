33f0826fb7c29509c0869e6084449dfb87863fcb
Dariya Nikitin <dn620@ic.ac.uk>
2022-01-06 12:21:57 +0000

    tried to not feed in initial params to pars

--- R/onevax_xvwrh.R
+++ R/onevax_xvwrh.R
@@ -13,7 +13,6 @@ initial_params_xvwrh <- function(pars, coverage = 0, hes = 0) {
   n_vax <- 5
   willing <- 1-hes
   cov <- c((willing * (1-coverage)), (willing*coverage), 0, 0, hes)
-  browser() 
   initial_params(pars, n_vax, cov)
 }
 
@@ -132,7 +131,12 @@ run_onevax_xvwrh <- function(tt, gono_params, init_params = NULL,
   
   if (is.null(init_params)) {
     
-    pars <- lapply(gono_params, model_params)
+    demographic_params <- demographic_params()
+    ret <- c(demographic_params, gono_params)
+    vax_params0 <- vax_params0()
+    
+    pars <- c(ret, vax_params0)
+    
     ret <- Map(run, gono_params = gono_params, vax_params = vax_params,
                init_params = initial_params_xvwrh(pars = pars, hes = hes),                               #equilibrium isn't provided
                MoreArgs = list(tt = tt))
