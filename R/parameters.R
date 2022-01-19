a70de195c36841cdb776e68685b05ebd66993d27
Dariya Nikitin <61972511+Dariyanikitin@users.noreply.github.com>
2022-01-19 14:36:38 +0000

    neaten set_strategy
    Co-authored-by: Lilith Whittles <lwhittles@users.noreply.github.com>

--- R/parameters.R
+++ R/parameters.R
@@ -273,10 +273,8 @@ uptake <- c(primary_uptake, booster_uptake)
   } else if (strategy == "VoA(H)") {
     vod <- vod <- cbind(0, uptake)
   } else if (strategy == "VoD(L)+VoA(H)") {
-    vod <- matrix(c(primary_uptake, primary_uptake, booster_uptake,
-                    booster_uptake), nrow = 2, byrow = TRUE)
-    vos <- matrix(c(0, primary_uptake, 0, booster_uptake), nrow = 2,
-                  byrow = TRUE)
+    vod <- cbind(uptake, uptake)
+    vos <- cbind(0, uptake)
   } else if (strategy == "VoS") {
     vod <- matrix(rep(0, 4), nrow = 2)
     vos <- matrix(c(primary_uptake, primary_uptake, booster_uptake,
