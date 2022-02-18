d741e7e6f19931b148291ce4275d8dcb64ad6839
Lilith Whittles <lwhittles@users.noreply.github.com>
2022-02-18 09:51:37 +0000

    split vaccination into two components: those offered and those accepting. This means that uptake and ubptake of vbe are now seen directly by the odin model

--- inst/odin/model.R
+++ inst/odin/model.R
@@ -3,9 +3,12 @@
 ## 1: Low
 ## 2: High
 # j = Vaccination status
-## 1: Unvaccinated
-## 2: Bexsero
-## 3: Waned
+## 1: Unvaccinated (X)
+## Then optional from the below, depending on model structure
+## 2: Vaccinated (V)
+## 3: Waned (W)
+## 4: Revaccinated (R)
+## 5: Hesitant (H)
 
 n_group <- 2
 n_vax   <- user(1)
@@ -60,12 +63,17 @@ screened[, ] <- eta[i] * U[i, j]
 # vaccination
 ## time-varying switch
 vax_switch <- interpolate(vax_t, vax_y, "constant")
+
+## Number offered / accepting vaccine
 ## at screening
-n_vos[, , ] <- vos[i, j, k] * screened[i, k] * vax_switch
+n_oos[, , ] <- vos[i, j, k] * screened[i, k] * vax_switch
+n_vos[, , ] <- n_oos[i, j, k] * u[i, k]
 ## on diagnosis
-n_vod[, , ] <- vod[i, j, k] * n_TU[i, k] * vax_switch
-## on entry - no switch as background rate
-n_vbe[, , ] <- vbe[i, j, k] * entrants[i, k]
+n_ood[, , ] <- vod[i, j, k] * n_TU[i, k] * vax_switch
+n_vod[, , ] <- n_ood[i, j, k] * u[i, k]
+## on entry - no switch as background rate, adolescent uptake included in vbe
+n_obe[, , ] <- vbe[i, j, k] * entrants[i, k]
+n_vbe[, , ] <- n_obe[i, j, k] * u_vbe
 
 # waning
 wU[, , ] <- w[j, k] * U[i, k]
@@ -81,6 +89,7 @@ deriv(cum_diag_a[, ])     <- n_AT[i, j]
 deriv(cum_diag_s[, ])     <- n_ST[i, j]
 deriv(cum_treated[, ])    <- n_TU[i, j]
 deriv(cum_screened[, ])   <- screened[i, j]
+deriv(cum_offered[, ])    <- n_oos[i, j, j] + n_ood[i, j, j] + n_obe[i, j, j]
 deriv(cum_vaccinated[, ]) <- n_vos[i, j, j] + n_vod[i, j, j] + n_vbe[i, j, j]
 deriv(cum_vbe[, ]) <- n_vbe[i, j, j]
 
@@ -111,6 +120,7 @@ initial(cum_diag_a[, ])     <- 0
 initial(cum_diag_s[, ])     <- 0
 initial(cum_treated[, ])    <- 0
 initial(cum_screened[, ])   <- 0
+initial(cum_offered[, ])    <- 0
 initial(cum_vaccinated[, ]) <- 0
 initial(cum_vbe[, ]) <- 0
 
@@ -147,31 +157,32 @@ dim(cum_diag_a)     <- c(n_group, n_vax)
 dim(cum_diag_s)     <- c(n_group, n_vax)
 dim(cum_treated)    <- c(n_group, n_vax)
 dim(cum_screened)   <- c(n_group, n_vax)
+dim(cum_offered)    <- c(n_group, n_vax)
 dim(cum_vaccinated) <- c(n_group, n_vax)
 dim(cum_vbe)        <- c(n_group, n_vax)
 
 ## Parameters
-p[]     <- user()
-q[]     <- user()
+p[]     <- user() # Partner change rate in group L/H
+q[]     <- user() # Proportion in group L/H
 
-willing[] <- user()
-enr       <- user()
-exr       <- user()
-beta_t[]  <- user()
-eta_l_t[] <- user()
-eta_h_t[] <- user()
-epsilon   <- user()
-sigma     <- user()
-psi       <- user()
-nu        <- user()
-mu        <- user()
-rho       <- user()
+willing[] <- user() # Proportion willing to be vaccinated
+enr       <- user() # Rate of entry into population
+exr       <- user() # Rate of exit from population
+beta_t[]  <- user() # Time-varying rate of transmission
+eta_l_t[] <- user() # Time-varying rate of screening in group L
+eta_h_t[] <- user() # Time-varying rate of screening in group H
+epsilon   <- user() # Level of assortative mixing
+sigma     <- user() # Rate of developing symptoms
+psi       <- user() # Proportion of infections that develop symptoms
+nu        <- user() # Rate of natural recovery from asymptomatic infection
+mu        <- user() # Rate of treatment seeking
+rho       <- user() # Rate of recovery after treatment
 
 ## vaccination parameters
 # vaccination routes
-vbe[, , ] <- user()
-vos[, , ] <- user()
-vod[, , ] <- user()
+vbe[, , ] <- user() # vaccine map before entry
+vos[, , ] <- user() # vaccine map on screening
+vod[, , ] <- user() # vaccine map on diagnosis
 
 # vaccine effects
 vea[] <- user() # efficacy against acquisition
@@ -179,7 +190,9 @@ ved[] <- user() # efficacy against duration of infection
 ves[] <- user() # efficacy against symptoms
 vei[] <- user() # efficacy against infectiousness
 
-w[, ]    <- user()
+u_vbe    <- user() # uptake of VbE
+u[, ]    <- user() # Uptake matrix for VoD/VoD
+w[, ]    <- user() # Waning map
 vax_t[]  <- user()
 vax_y[]  <- user()
 
@@ -190,22 +203,26 @@ dim(eta_h_t) <- length(tt)
 
 dim(p)    <- n_group
 dim(q)    <- n_group
-dim(willing) <- n_vax
 dim(eta)  <- n_group
 dim(vea)  <- n_vax
 dim(ved)  <- n_vax
 dim(ves)  <- n_vax
 dim(vei)  <- n_vax
+dim(willing) <- n_vax
 dim(vbe)   <- c(n_group, n_vax, n_vax)
 dim(vod)   <- c(n_group, n_vax, n_vax)
 dim(vos)   <- c(n_group, n_vax, n_vax)
 dim(w)    <- c(n_vax, n_vax)
+dim(u)     <- c(n_group, n_vax)
 dim(vax_t) <- user()
 dim(vax_y) <- user()
 
 dim(n_vbe) <- c(n_group, n_vax, n_vax)
 dim(n_vos) <- c(n_group, n_vax, n_vax)
 dim(n_vod) <- c(n_group, n_vax, n_vax)
+dim(n_obe) <- c(n_group, n_vax, n_vax)
+dim(n_oos) <- c(n_group, n_vax, n_vax)
+dim(n_ood) <- c(n_group, n_vax, n_vax)
 dim(wU)   <- c(n_group, n_vax, n_vax)
 dim(wI)   <- c(n_group, n_vax, n_vax)
 dim(wA)   <- c(n_group, n_vax, n_vax)
