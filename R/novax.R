286c570ce16129ea9007b5ae46f7408480600894
Lilith Whittles <lwhittles@users.noreply.github.com>
2022-02-18 10:54:24 +0000

    move willing to top for consistency

--- R/novax.R
+++ R/novax.R
@@ -7,6 +7,7 @@ vax_params0 <- function() {
   n_vax <- 1
   v <- array(0, dim = c(n_group, n_vax, n_vax))
   list(n_vax = 1,
+       willing = 1,
        u_vbe = 0,
        u = matrix(0, n_group, n_vax),
        vbe = v,
@@ -16,7 +17,6 @@ vax_params0 <- function() {
        ved = 0,
        ves = 0,
        vei = 0,
-       willing = 1,
        w = as.matrix(0),
        vax_t = c(0, 99),
        vax_y = c(0, 0))
