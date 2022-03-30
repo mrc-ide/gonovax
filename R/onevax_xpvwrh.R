f61487b262f2e3ad95575e516f653bdc9b1854bd
Dariya Nikitin <dn620@ic.ac.uk>
2022-03-30 15:10:49 +0100

    lint

--- R/onevax_xpvwrh.R
+++ R/onevax_xpvwrh.R
@@ -27,22 +27,22 @@ initial_params_xpvwrh <- function(pars, coverage_p = 0, coverage_v = 0,
   p_init <- willing * coverage_p
   v_init <- willing * coverage_v
   cov <- c(x_init, p_init, v_init, 0, 0, hes)
-  
+
   stopifnot(length(cov) == n_vax)
   stopifnot(sum(cov) == 1)
-  
-  U0 <- I0 <- A0 <- S0 <- T0 <- array(0, c(2, n_vax))
+
+    U0 <- I0 <- A0 <- S0 <- T0 <- array(0, c(2, n_vax))
   # separate into 1:low and 2:high activity groups and by coverage
   N0 <- pars$N0 * outer(pars$q, cov)
   # set initial asymptomatic prevalence in each group (X AND H)
   A0[, 1] <- round(N0[, 1] * c(pars$prev_Asl, pars$prev_Ash))
   A0[, n_vax] <- round(N0[, n_vax] * c(pars$prev_Asl, pars$prev_Ash))
-  
+
   # set initial uninfecteds
   U0 <- round(N0) - A0
-  
+
   list(U0 = U0, I0 = I0, A0 = A0, S0 = S0, T0 = T0)
-  
+
 }
 
 
@@ -75,7 +75,7 @@ vax_params_xvwrh <- function(vea = 0, vei = 0, ved = 0, ves = 0,
                              r1 = 0, r1r2 = 0,
                              booster_uptake = r1r2, strategy = NULL,
                              vbe = 0, t_stop = 99, hes = 0) {
-  
+
   assert_scalar_unit_interval(vea_p)
   assert_scalar_unit_interval(vei_p)
   assert_scalar_unit_interval(ved_p)
@@ -96,7 +96,7 @@ vax_params_xvwrh <- function(vea = 0, vei = 0, ved = 0, ves = 0,
   assert_scalar_unit_interval(booster_uptake)
   assert_scalar_unit_interval(vbe)
   assert_scalar_positive(t_stop)
-  
+
   # waned partially-vaccinated individuals (P) move back to the non-vaccinated
   # stratum (X) and are considered immunologically naive. They are eligible
   # for another round of partial vaccination or full vaccination
@@ -105,34 +105,34 @@ vax_params_xvwrh <- function(vea = 0, vei = 0, ved = 0, ves = 0,
   # stratum (R)
   # a proportion of all 'n' exist only in the hesitant compartment (H)
   # there is no movement between the willing (X, P, V, W, R) and hesitant (H)
-  
+
   # 1:X -> 3:V -> 4:W <-> 5:R
   # and
   # 1:X <-> 2:P
-  
+
   i_eligible <- c(1, 4)                # X(1) and W(4) eligible for vaccination
   i_w <- c(1, 4, 4)                   # Waned vaccinees move to X(1) and W(4)
   i_v <- c(2, 3, 5)                    # P(2), V(3), and R(5) are protected
-  
+
   #number of compartments
   n_vax <- 6
   n_group <- 2
-  
+
   # ensure duration is not divided by 0
   ved <- min(ved, 1 - 1e-10)
   ved_revax <- min(ved_revax, 1 - 1e-10)
-  
+
   # If uptake of VbE > 0 consider that all adolescents are offered vaccine
   p <- set_strategy(strategy, vbe > 0)
-  
+
   # set up uptake matrix rows = groups, columns = vaccine strata
-  # tells us where vaccinated individuals are going to be pulled from 
+  # tells us where vaccinated individuals are going to be pulled from
 
   u <- matrix(0, n_group, n_vax)
   u[, i_eligible[1]] <- r1r2 + r1
   u[, i_eligible[2]] <- booster_uptake
 
-  
+
   list(n_vax   = n_vax,
        willing = c((1 - hes), 0, 0,  0, 0, hes),
        u       = u,
@@ -143,10 +143,10 @@ vax_params_xvwrh <- function(vea = 0, vei = 0, ved = 0, ves = 0,
                                           r1 = r1, r1r2 = r1r2),
        vos     = create_vax_map_branching(n_vax, p$vos, i_eligible, i_v,
                                           r1 = r1, r1r2 = r1r2),
-       vea     = c(0,vea_p , vea, 0, vea_revax, 0),
-       vei     = c(0,vei_p , vei, 0, vei_revax, 0),
-       ved     = c(0,ved_p , ved, 0, ved_revax, 0),
-       ves     = c(0,ves_s , ves, 0, ves_revax, 0),
+       vea     = c(0, vea_p, vea, 0, vea_revax, 0),
+       vei     = c(0, vei_p, vei, 0, vei_revax, 0),
+       ved     = c(0, ved_p, ved, 0, ved_revax, 0),
+       ves     = c(0, ves_s, ves, 0, ves_revax, 0),
        w       = create_waning_map_branching(n_vax, i_v,
                                    i_w, 1 / c(dur_p, dur_v, dur_revax)),
        vax_t   = c(0, t_stop),
@@ -166,15 +166,17 @@ vax_params_xvwrh <- function(vea = 0, vei = 0, ved = 0, ves = 0,
 ##' @return an array of the mapping
 
 create_waning_map_branching <- function(n_vax, i_v, i_w, z) {
-  
+
   stopifnot(z > 0)
   stopifnot(length(z) %in% c(1, length(i_v)))
   stopifnot(length(i_w) == 3)
 
   # set up waning map
   w <- array(0, dim = c(n_vax, n_vax))
-  
-  for (i in 1:length(z)) {
+
+  loop <- length(i_v)
+
+  for (i in 1:loop) {
     w[i_w[i], i_v[i]] <- z[i]
     w[i_v[i], i_v[i]] <- -z[i]
   }
@@ -196,34 +198,34 @@ create_waning_map_branching <- function(n_vax, i_v, i_w, z) {
 ##' @return an array of the mapping
 
 create_vax_map_branching <- function(n_vax, v, i_u, i_v, r1, r1r2) {
-  
+
   # ensure vaccine input is of correct length
   n_group <- 2
   stopifnot(length(v) == n_group)
   stopifnot(all(v %in% c(0, 1)))
   stopifnot(max(i_u, i_v) <= n_vax)
-  
+
   # tweak eligibility , repeat over stratum 1 column 1 for ease
   i_u <- c(1, i_u)
-  
+
   # set up vaccination matrix
   vax_map <- array(0, dim = c(n_group, n_vax, n_vax))
-  
+
   for (i in seq_along(i_u)) {
     vax_map[, i_u[i], i_u[i]] <-  v
     vax_map[, i_v[i], i_u[i]] <- -v
 
-  }                                        #maybe split this up otherwise  getting negatives for , , 4 as well! 
-  
+  }
+
   #obtain proportions
 
   tot <- r1r2 + r1
   prop_full <- r1r2 / tot
-  prop_part <- r1 / tot             #multiply these proportions through to , , 1
-  
+  prop_part <- r1 / tot
+
   vax_map[, 2, 1] <- vax_map[, 2, 1] * prop_part
   vax_map[, 3, 1] <- vax_map[, 3, 1] * prop_full
-  
+
   vax_map
 }
 
@@ -234,7 +236,7 @@ create_vax_map_branching <- function(n_vax, v, i_u, i_v, r1, r1r2) {
 ##' from initialisation or from equilibrium, those with waned vaccines are
 ##' eligible for revaccination (R), and return to the R stratum, those with
 ##' waned partial vaccines return to the unvaccinated stratum (X) and considered
-##' immunologically naive. A user defined proportion of the population is 
+##' immunologically naive. A user defined proportion of the population is
 ##' vaccine hesitant and is never vaccinated. Full acciantion (V) with 2 doses
 ##' gives maximum protection whereas partial vaccinaiton with 1 dose (P) gives
 ##' less.
@@ -275,7 +277,7 @@ create_vax_map_branching <- function(n_vax, v, i_u, i_v, r1, r1r2) {
 ##' @param booster_uptake scalar or numeric vector with same length as
 ##'  'gono_params' giving proportion of population undertaking booster
 ##'  vaccination after primary vaccination protection has waned.
-##'   Defaults to supplied value of `r1r2` 
+##'   Defaults to supplied value of `r1r2`
 ##' @inheritParams run_onevax_xvwv
 ##' @return A list of transformed model outputs
 ##' @export
@@ -290,14 +292,14 @@ run_onevax_xvwrh <- function(tt, gono_params, init_params = NULL,
                              r1 = 0, r1r2 = 0,
                              booster_uptake = r1r2, strategy = NULL,
                              t_stop = 99, hes = 0) {
-  
+
   stopifnot(all(lengths(list(booster_uptake, r1r2, r1, vea, vei,
                              ved, ves, vea_revax, vei_revax, ved_revax,
                              vea_p, vei_p, ves_p, ved_p,
                              dur_v, dur_p,
                              ves_revax, dur_revax)) %in%
                   c(1, length(gono_params))))
-  
+
   vax_params <- Map(vax_params_xpvwrh, r1 = r1, r1r2 = r1r2,
                     booster_uptake = booster_uptake, dur_v = dur_v,
                     vea = vea, vei = vei, ved = ved, ves = ves,
@@ -307,18 +309,17 @@ run_onevax_xvwrh <- function(tt, gono_params, init_params = NULL,
                     ved_revax = ved_revax, ves_revax = ves_revax, hes = hes,
                     MoreArgs = list(strategy = strategy,
                                     t_stop = t_stop, vbe = vbe))
-  
+
   if (is.null(init_params)) {
     pars <- lapply(gono_params, model_params)
     init_params <- lapply(pars, initial_params_xpvwrh, hes = hes)
   }
-  
+
   ret <- Map(run, gono_params = gono_params, vax_params = vax_params,
              init_params = init_params,
              MoreArgs = list(tt = tt))
-  
+
   # name outputs
   ret <- lapply(ret, name_outputs, c("X", "P", "V", "W", "R", "H"))
   ret
 }
-
