140a05e1e3eec23de0f80904f353b8bead90d295
Dariya Nikitin <dn620@ic.ac.uk>
2022-04-11 15:17:54 +0100

    tests now pass for xvw, and xvwv

--- R/onevax_xvw.R
+++ R/onevax_xvw.R
@@ -56,10 +56,19 @@ vax_params_xvw <- function(vea = 0, vei = 0, ved = 0, ves = 0,
 
   # If uptake of VbE > 0 consider that all adolescents are offered vaccine
   p <- set_strategy(strategy, vbe > 0)
+  
+  # set up uptake matrix rows = groups, columns = vaccine strata
+  u <- array(0, dim = c(n_group, n_vax, n_vax))
+  
+  for (i in seq_along(i_eligible)) {
+    u[, i_eligible[i], i_eligible[i]] <- uptake
+    u[, i_v[i], i_eligible[i]]      <- uptake
+    
+  }
 
   list(n_vax   = n_vax,
        willing = c(1, 0, 0),
-       u       = matrix(uptake, n_group, n_vax),
+       u       = u,
        u_vbe   = vbe,
        vbe     = create_vax_map(n_vax, p$vbe, i_eligible, i_v),
        vod     = create_vax_map(n_vax, p$vod, i_eligible, i_v),
