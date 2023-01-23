e6c411be8ff1ed48dcb1bc32d407f90739e22130
Dariya Nikitin <dn620@ic.ac.uk>
2023-01-23 12:38:01 +0000

    added an additional dimension to w and u to conform to dimensions expected by odin model

--- R/novax.R
+++ R/novax.R
@@ -3,9 +3,10 @@
 ##' @title create vaccination parameters for use in novax model (null)
 ##' @return A list parameters in the model input format
 vax_params0 <- function() {
+  n_erlang <- 1
   n_group <- 2
   n_vax <- 1
-  v <- array(0, dim = c(n_group, n_vax, n_vax))
+  v <- array(0, dim = c(n_group, n_vax, n_vax, n_erlang))
   list(n_vax = 1,
        willing = 1,
        u_vbe = 0,
@@ -17,7 +18,7 @@ vax_params0 <- function() {
        ved = 0,
        ves = 0,
        vei = 0,
-       w = as.matrix(0),
+       w = array(0, dim = c(n_vax, n_vax, n_erlang)),
        vax_t = c(0, 99),
        vax_y = c(0, 0))
 }
