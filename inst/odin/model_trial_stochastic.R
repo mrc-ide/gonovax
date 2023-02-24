779f473d2150e3553bc39f9a4e52302ecc982432
Dariya Nikitin <dn620@ic.ac.uk>
2023-02-24 13:29:08 +0000

    discret stochastic compiles!!!!!!!!!!! now need to sort out waning

--- inst/odin/model_trial_stochastic.R
+++ inst/odin/model_trial_stochastic.R
@@ -15,66 +15,41 @@ eta[2] <- eta_h
 
 # individual probabilities of transitioning between infection states
 p_UI[, ] <- 1 - exp(-(lambda * (1 - vea[j])))
-p_IAS[, ] <- 1 - exp(-sigma)
-p_ATU[, ] <- 1 - exp(-(eta[i] +  nu / (1 - ved[j])))
 p_ST[, ] <- 1 - exp(-mu)
 p_TU[, ] <- 1 - exp(-rho)
 
+p_A_or_S[, ] <- 1 - exp(-sigma)
+
+r_AT[, ] <- eta[i]         
+r_AU[, ] <- nu / (1 - ved[j]) 
+p_T_or_U[, ] <- 1 - exp(-(r_AT[i, j] + r_AU[i, j]))
+
 # draws from binomial distributions for numbers changing between compartments
-# note there will be two draws for I (moving to A or S) 
-# and A (moving to U or T)
 n_UI[, ] <- rbinom(U[i, j], p_UI[i, j])
 n_ST[, ] <- rbinom(S[i, j], p_ST[i, j])
 n_TU[, ] <- rbinom(T[i, j], p_TU[i, j])
 
- #split from I to A/S
-  #assign individual probabilities of transition to A or S from I
-  p_A_or_S[, , 1] <- (1 - (1 - ves[j]) * psi)    # to A                #lilith has this as one dimension only 
-  p_A_or_S[, , 2] <- (1 - ves[j]) * psi          # to S
-  dim(p_A_or_S) <- c(n_group, n_vax, 2)                                 # i htink this should just be a vector of length 2 /
- 
-  #draw from binomial distribution the number leaving I
-  n_IAS[, ] <- rbinom(I[i,j], p_IAS[i, j])        
-  #draw from multinomial distribution the number entering A and S
-  n_A_or_S[, ] <- rmultinom(n_IAS[i, j], p_A_or_S[i, j, ])       #number moving to A and S 
-  dim(n_A_or_S) <- c(n_group, n_vax, 2)                          #check this because we have 2D and vinette has 1D
- 
-  #pull out number entering A and number entering S
-  n_IA[, ] <- n_A_or_S[, , 1]     
-  n_IS[, ] <- n_A_or_S[, , 2]
- 
- #split from A to U/T
- #assign individual probabilities of transition to T or U from A
-  r_AT[, ] <- eta[i]           #to T
-  r_AU[, ] <- nu / (1 - ved[j]) #to U
-  p_T_or_U[, ] <- 1 - exp(-(r_AT[i, j] + r_AU[i, j]))
-  
-  dim(p_T_or_U) <- c(n_group, n_vax)          # still unsure of this 
-  
-  #draw for binomial distribution the number leaving A (combined probability)
-  n_AUT[, ] <- rbinom(A[i, j], p_T_or_U[i, j])
-  
-  #make separate draws for A->T and A->U                  #why is the first draw with a probability and the second with a proportion (weighted rate or something?)
-  n_AT[, ] <- rbinom(n_AUT, r_AT[i,j]/(r_AT[i,j] + r_AU[i, j]))
-  n_AU[, ] <- rbinom(n_AUT, r_AU[i,j]/(r_AT[i,j] + r_AU[i, j]))
+n_IAS[, ] <- rbinom(I[i,j], p_A_or_S[i, j])    
+n_IA[, ] <- rbinom(n_IAS[i, j], (1 - ves[j]) * psi) 
+n_IS[, ] <- n_IAS[i, j] - n_IA[i, j]
 
-
-
-########## work out dimmesions and why missing dim() error isn't satisfied when  we add dims? 
+n_AUT[, ] <- rbinom(A[i, j], p_T_or_U[i, j])
+n_AT[, ] <- rbinom(n_AUT[i, j], r_AT[i,j]/(r_AT[i,j] + r_AU[i, j]))
+n_AU[, ] <- n_AUT[i, j] - n_AT[i, j]
+  
 ## Core equations for transitions between compartments:
-
 update(U[, ]) <- U[i, j] - n_UI[i, j] +
   n_AU[i, j] + n_TU[i, j]  + sum(wU[i, j, ])
 
 update(I[, ]) <- I[i,j] + n_UI[i, j] - n_IAS[i, j] + sum(wI[i, j, ])
 
 update(A[, ]) <- A[i,j] + n_IA[i, j] -
-               n_AT[i, j] - n_AU[i, j] + sum(wA[i, j, ])
+               n_AUT[i, j] + sum(wA[i, j, ])
 
 update(S[, ]) <- S[i,j] + n_IS[i, j] -
   n_ST[i, j]  + sum(wS[i, j, ])
 
-update(T[, ]) <- T[i,j] + n_ST[i, j] + n_AT[i, j]  - n_TU[i, j] +
+update(T[, ]) <- T[i,j] + n_ST[i, j] + n_AT[i, j] - n_TU[i, j] +
   sum(wT[i, j, ])
 
 ## Update population size
@@ -138,11 +113,24 @@ dim(T0) <- c(n_group, n_vax)
 dim(N)  <- c(n_group, n_vax)
 
 dim(n_UI)     <- c(n_group, n_vax)
+dim(n_IAS)    <- c(n_group, n_vax)
+dim(n_IA)     <- c(n_group, n_vax)
+dim(n_IS)     <- c(n_group, n_vax)
+dim(n_AUT)    <- c(n_group, n_vax)
 dim(n_AT)     <- c(n_group, n_vax)
 dim(n_AU)     <- c(n_group, n_vax)
 dim(n_ST)     <- c(n_group, n_vax)
 dim(n_TU)     <- c(n_group, n_vax)
 dim(screened) <- c(n_group, n_vax)
+
+dim(p_UI)     <- c(n_group, n_vax)
+dim(p_A_or_S) <- c(n_group, n_vax)
+dim(p_ST)     <- c(n_group, n_vax)
+dim(p_T_or_U) <- c(n_group, n_vax)   
+dim(p_TU)     <- c(n_group, n_vax)
+
+dim(r_AT) <- c(n_group, n_vax)        
+dim(r_AU) <- c(n_group, n_vax)
 
 dim(cum_incid)      <- c(n_group, n_vax)
 dim(cum_diag_a)     <- c(n_group, n_vax)
