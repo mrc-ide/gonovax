3fde0572ee5358b979217d5daf6df173b33a7639
Dariya Nikitin <dn620@ic.ac.uk>
2023-06-09 11:33:45 +0100

    lintr

--- R/support.R
+++ R/support.R
@@ -119,11 +119,11 @@ extract_flows_trial <- function(y) {
                   "N")
   cumulative_flows <- lapply(flow_names, function(x) t(aggregate(y, x)))
   names(cumulative_flows) <- flow_names
-  
+
   strata <- dimnames(y[[1]]$N)[[3]] # extract strata names
   n_diag_hist <- sum(grepl("W", strata)) # count diag hist categories
   n_erlang <- sum((grepl("V", strata))) / n_diag_hist # count erlang
-  
+
   idx <- stratum_index_xvw_trial(n_erlang, n_diag_rec)
   idx$never_diag <- seq(idx$V[1], by = n_diag_rec, length.out = n_erlang + 1)
 
