9a4c0a73fa63b63558593a844e6f2dac16399bb3
Dariya Nikitin <dn620@ic.ac.uk>
2024-03-12 18:16:18 +0800

    changed how ved affects nu, now rate of clearance is capped at the rate of care seeking

--- inst/odin/model_trial.R
+++ inst/odin/model_trial.R
@@ -28,9 +28,10 @@ deriv(T[, ]) <- n_ST[i, j] + n_AT[i, j]  - n_TU[i, j] +
 ## Update population size
 N[, ] <- U[i, j] + I[i, j] + A[i, j] + S[i, j] + T[i, j]
 
+#number transitioning between infection states
 n_UI[, ]     <- lambda * (1 - vea[j]) * U[i, j]  # force of infection constant
 n_AT[, ]     <- eta * A[i, j]                 # in trial model
-n_AU[, ]     <- nu / (1 - ved[j]) * A[i, j]
+n_AU[, ]     <- (ved[j] * (mu - nu) + nu) * A[i,j]
 n_ST[, ]     <- mu * S[i, j]
 n_TU[, ]     <- rho * T[i, j]
 screened[, ] <- eta * U[i, j]
