594e0152bac9f9c661081380fb70789cd79b1f9e
Dariya Nikitin <dn620@ic.ac.uk>
2024-03-20 11:27:59 +1000

    added if statement for when mu or nu = 0 to prevent division by zero

--- inst/odin/model.R
+++ inst/odin/model.R
@@ -56,10 +56,12 @@ Np[]    <- sum(N[i, ]) * p[i]
 foi_LH[] <- prop_C[i] * Np[i] / sum(Np[])
 lambda[] <- p[i] * beta * (epsilon * prop_C[i] + (1 - epsilon) * sum(foi_LH[]))
 
-
 n_UI[, ]     <- lambda[i] * (1 - vea[j]) * U[i, j]
 n_AT[, ]     <- eta[i] * A[i, j]
-n_AU[, ]     <- 1 / (ved[j] * (1 / mu - 1 / nu) + 1 / nu) * A[i, j]
+n_AU[, ]     <- if(mu == 0 || nu == 0) (nu / (1 - ved[j]) *
+                                          A[i, j]) else 1 / (ved[j] *
+                                        ((1 / mu) - (1 / nu)) + (1 / nu)) *
+                                        A[i, j]
 n_ST[, ]     <- mu * S[i, j]
 n_TU[, ]     <- rho * T[i, j]
 screened[, ] <- eta[i] * U[i, j]
