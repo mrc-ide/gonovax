13f27085fdc1035b3641baf3196efbc5c3310fd3
Dariya Nikitin <dn620@dide.local>
2024-08-07 12:11:16 +0100

    added ODE for pye over time in odin deterministic trial model

--- inst/odin/model_trial.R
+++ inst/odin/model_trial.R
@@ -34,6 +34,7 @@ n_AU[, ]     <- nu / (1 - ved[j]) * A[i, j]
 n_ST[, ]     <- mu * S[i, j]
 n_TU[, ]     <- rho * T[i, j]
 screened[, ] <- eta * U[i, j]
+pye_trial[, ] <- U[i, j] + I[i ,j] + A[i ,j] + S[i ,j]
 
 # mechanism to record number of times infected by moving diagnosed
 # individuals into stratum with the relevant diagnosis history
@@ -51,11 +52,13 @@ wT[, , ] <- w[j, k] * T[i, k]
 
 ## outputs
 
-deriv(cum_incid[, ])      <- n_UI[i, j]
-deriv(cum_diag_a[, ])     <- n_AT[i, j]
-deriv(cum_diag_s[, ])     <- n_ST[i, j]
-deriv(cum_treated[, ])    <- n_TU[i, j]
-deriv(cum_screened[, ])   <- screened[i, j]
+deriv(cum_incid[, ])             <- n_UI[i, j]
+deriv(cum_diag_a[, ])            <- n_AT[i, j]
+deriv(cum_diag_s[, ])            <- n_ST[i, j]
+deriv(cum_treated[, ])           <- n_TU[i, j]
+deriv(cum_screened[, ])          <- screened[i, j]
+deriv(cum_pye_trial_pov[, ]) <- pye_trial[i, j]
+deriv(cum_pye_true[, ])      <- U[i, j]
 
 # aggregated time series for fitting mcmc
 output(tot_treated) <- sum(cum_treated)
@@ -80,6 +83,8 @@ initial(cum_diag_a[, ])     <- 0
 initial(cum_diag_s[, ])     <- 0
 initial(cum_treated[, ])    <- 0
 initial(cum_screened[, ])   <- 0
+initial(cum_pye_trial_pov[, ]) <- 0
+initial(cum_pye_true[, ]) <- 0
 
 # set up dimensions of compartments
 dim(U) <- c(n_group, n_vax)
@@ -96,20 +101,23 @@ dim(T0) <- c(n_group, n_vax)
 
 dim(N)  <- c(n_group, n_vax)
 
-dim(n_UI)     <- c(n_group, n_vax)
-dim(n_AT)     <- c(n_group, n_vax)
-dim(n_AU)     <- c(n_group, n_vax)
-dim(n_ST)     <- c(n_group, n_vax)
-dim(n_TU)     <- c(n_group, n_vax)
-dim(screened) <- c(n_group, n_vax)
+dim(n_UI)       <- c(n_group, n_vax)
+dim(n_AT)       <- c(n_group, n_vax)
+dim(n_AU)       <- c(n_group, n_vax)
+dim(n_ST)       <- c(n_group, n_vax)
+dim(n_TU)       <- c(n_group, n_vax)
+dim(screened)   <- c(n_group, n_vax)
+dim(pye_trial)  <- c(n_group, n_vax)
 dim(n_diag_rec) <- c(n_group, n_vax, n_vax)
 dim(diag_rec)   <- c(n_group, n_vax, n_vax)
 
-dim(cum_incid)      <- c(n_group, n_vax)
-dim(cum_diag_a)     <- c(n_group, n_vax)
-dim(cum_diag_s)     <- c(n_group, n_vax)
-dim(cum_treated)    <- c(n_group, n_vax)
-dim(cum_screened)   <- c(n_group, n_vax)
+dim(cum_incid)           <- c(n_group, n_vax)
+dim(cum_diag_a)          <- c(n_group, n_vax)
+dim(cum_diag_s)          <- c(n_group, n_vax)
+dim(cum_treated)         <- c(n_group, n_vax)
+dim(cum_screened)        <- c(n_group, n_vax)
+dim(cum_pye_trial_pov)   <- c(n_group, n_vax) 
+dim(cum_pye_true)        <- c(n_group, n_vax)
 
 ## Parameters
 
