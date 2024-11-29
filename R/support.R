498785d368b50a8bc8e15e72fa6d8a2933d6d636
tsleng93 <51326999+tsleng93@users.noreply.github.com>
2024-11-29 11:51:00 +0000

    add cum_lifeyears_U to named outputs

--- R/support.R
+++ R/support.R
@@ -312,7 +312,7 @@ name_outputs <- function(res, strata_names) {
   state_names <- c("U", "I", "A", "S", "T", "N",
                    "cum_incid", "cum_diag_a", "cum_diag_s",
                    "cum_treated", "cum_screened", "cum_vaccinated",
-                   "cum_offered", "cum_vbe")
+                   "cum_offered", "cum_vbe", "cum_lifeyears_U")
 
   for (nm in state_names) {
     dimnames(res[[nm]]) <- list(NULL, group_names, strata_names)
